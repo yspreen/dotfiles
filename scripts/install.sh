e49d63ddbf847f5941c704835d793f09d4ff6c9a
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-02-21 20:45:46 -0500

    Make username parametric.

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,4 +5,4 @@ set -e
 cd ~
 sh <(curl -L https://nixos.org/nix/install)
 nix-shell -p git --run 'git clone https://github.com/yspreen/dotfiles.git dotfiles'
-nix run nix-darwin --extra-experimental-features 'nix-command flakes' -- switch --flake ~/dotfiles/nix#spreen
+nix run nix-darwin --extra-experimental-features 'nix-command flakes' -- switch --impure --flake ~/dotfiles/nix#spreen
