fd037c10f0e2bb602ec735213f8f8dbc1ea9e65b
Yannick <yannick.spreen@gmail.com>
2025-02-21 19:19:35 -0500

    Fix build cmd.

--- .config/sketchybar/plugins/clisolarwallpaper/build.sh
+++ .config/sketchybar/plugins/clisolarwallpaper/build.sh
@@ -9,5 +9,5 @@ rm -rf build
 # Build the Xcode project using the Release configuration and custom derived data path
 xcodebuild -project clisolarwallpaper.xcodeproj -scheme clisolarwallpaper -configuration Release -derivedDataPath build
 # Remove any existing built bundle and copy the new one
-rm -rf clisolarwallpaper/clisolarwallpaper.app
+rm -rf ./clisolarwallpaper.app
 mv build/Build/Products/Release/clisolarwallpaper.app ./clisolarwallpaper.app
