80876228d8ef550dbcf12cf4ec511095026b9830
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-02-25 09:48:40 -0500

    Decrypt ssh during install.

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,4 +5,5 @@ set -e
 cd ~
 sh <(curl -L https://nixos.org/nix/install)
 nix-shell -p git --run 'git clone https://github.com/yspreen/dotfiles.git dotfiles'
+~/dotfiles/scripts/decrypt-ssh.sh
 nix run nix-darwin --extra-experimental-features 'nix-command flakes' -- switch --impure --flake ~/dotfiles/nix#spreen
