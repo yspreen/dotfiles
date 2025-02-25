e613fb7b4191313c2e1a2faca460f73a06894f56
Yannick <yannick.spreen@gmail.com>
2025-02-24 20:40:11 -0500

    Use new highlight color.

--- .config/sketchybar/plugins/aerospacer.sh
+++ .config/sketchybar/plugins/aerospacer.sh
@@ -4,7 +4,7 @@ if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
     if [ -f ~/.aerospaceservice ]; then
         bg_color=0x62EA2B58 # Color for service mode
     else
-        bg_color=0x3640ffff # Color for main mode
+        bg_color=0x20ffffff # Color for main mode
     fi
     sketchybar --set $NAME background.drawing=on background.color=$bg_color
 else
