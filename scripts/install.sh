6c5ba16d3b788cfc333ceb59eca997a3f8489dc1
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-03-02 12:45:31 -0500

    Add prompt pre install.

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,6 +2,23 @@
 
 set -e
 
+# print "WARNING" in dark yellow
+echo -e "\033[33;1m⚠️ ======== *** WARNING *** ======== ⚠️\033[0m"
+
+echo "We are about to replace your entire system configuration. This will install nix and overwrite your nix installation. This script will override all home directory configuration files. We will also uninstall all home brew packages. You probably don't want to run these commands if your name is not Nick. To familiarize yourself more with what will be installed please check https://github.com/yspreen/dotfiles"
+
+# print "WARNING" in dark yellow
+echo -e "\033[33;1m⚠️ ======== *** WARNING *** ======== ⚠️\033[0m"
+
+echo
+
+# Insert confirmation prompt before running any commands
+read -p "Are you sure you want to run these commands? (y/N): " answer
+if [[ $answer != "y" && $answer != "Y" ]]; then
+    echo "Aborted"
+    exit 1
+fi
+
 cd ~
 sh <(curl -L https://nixos.org/nix/install)
 nix-shell -p git --run 'git clone https://github.com/yspreen/dotfiles.git dotfiles'
