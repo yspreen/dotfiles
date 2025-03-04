d913eb17ea9ddd33b5fc59db3a7514c7b5c44fef
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-03-04 15:56:09 -0500

    Fix charging icon.

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
-CHARGING="$(pmset -g batt | grep 'AC Power' && echo 1 || echo 0)"
+CHARGING="$(pmset -g batt | grep 'AC Power' >/dev/null && echo 1 || echo 0)"
 
 sketchybar --set "$NAME" label="$([ $CHARGING = 1 ] && echo "⚡ " || printf '')${PERCENTAGE}%"
