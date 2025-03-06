39baa005a634a27d3f713230562edc6c0ca7071e
Yannick <yannick.spreen@gmail.com>
2025-03-06 13:06:58 -0500

    Add wallpaper retry.

--- .config/sketchybar/plugins/battery.sh
+++ .config/sketchybar/plugins/battery.sh
@@ -3,4 +3,4 @@
 PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
 CHARGING="$(pmset -g batt | grep 'AC Power' >/dev/null && echo 1 || echo 0)"
 
-sketchybar --set "$NAME" label="$([ $CHARGING = 1 ] && echo "⚡ " || printf '')${PERCENTAGE}%"
+sketchybar --set "$NAME" y_offset=$([ $CHARGING = 1 ] && printf '2' || printf '0.5') label="$([ $CHARGING = 1 ] && echo "⚡ " || printf '')${PERCENTAGE}%"
