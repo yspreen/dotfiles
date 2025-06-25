9fe47d480cc5dcddd68a334d6b63f6b10e9062d9
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-06-25 12:05:47 -0400

    Remove sketchybar.

--- .config/sketchybar/plugins/battery.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
-CHARGING="$(pmset -g batt | grep 'AC Power' >/dev/null && echo 1 || echo 0)"
-
-sketchybar --set "$NAME" y_offset=$([ $CHARGING = 1 ] && printf '2' || printf '0.5') label="$([ $CHARGING = 1 ] && echo "⚡ " || printf '')${PERCENTAGE}%"
--- .config/sketchybar/plugins/lock.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-osascript -e 'tell application "System Events" to key code 12 using {control down, command down}'
